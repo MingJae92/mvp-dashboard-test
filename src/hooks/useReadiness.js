8acf3c884a241f57540addd76c7bce785ed64b6a
Ming Chi Wong <wong_mingchi@yahoo.co.uk>
2026-01-21 02:22:36 +0000

    useReadiness hook updated

--- src/hooks/useReadiness.js
+++ src/hooks/useReadiness.js
@@ -11,12 +11,21 @@ export function useReadiness(employeeId) {
 
     setLoading(true)
     setError(null)
+    setData(null) // reset previous data
 
     axios
       .get(
         `https://fantastic-fortnight-7r95qxrxg736qw-8001.app.github.dev/readiness?employeeId=${employeeId}`
       )
-      .then(res => setData(res.data[0]))
+      .then(res => {
+        // Only set data if something exists
+        if (res.data && res.data.length > 0) {
+          setData(res.data[0])
+        } else {
+          setData(null)
+          setError('No readiness data found for this employee')
+        }
+      })
       .catch(() => setError('Failed to load readiness data'))
       .finally(() => setLoading(false))
   }, [employeeId])
